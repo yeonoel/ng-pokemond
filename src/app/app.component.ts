e794308db45e56500ff56ad8080a4732bb6da309
yeo noel <yeopevroguinoel@gmail.com>
2024-12-19 00:43:04 +0000

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1> Welcome to {{title}}! </h1>`
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonsList = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+
+  ngOnInit(){
+    console.table(this.pokemonsList)
+  }
+
+  selectPokemon(pokemonId: String) {
+    const pokemon: Pokemon|undefined = this.pokemonsList.find(pokemon => pokemon.id === +pokemonId)
+    console.log(`VOus avez cliqué sur le pokémon  ${pokemon?.name}`)
+    this.pokemonSelected = pokemon;
+  }
+
 }
+
---
+++ src/app/pokemon.ts
@@ -0,0 +1,10 @@
+/* Chapitre 2 : Les Composants */
+export class Pokemon {
+    id: number;
+    hp: number;
+    cp: number;
+    name: string;
+    picture: string;
+    types: Array<string>;
+    created: Date;
+  }
